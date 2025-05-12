6bf85f504861c8364511c3ded6d2442368251208
YeruhamMendelson <y0583207549@gmail.com>
2025-05-12 15:46:26 +0300

    Creating the file

---
+++ TheSeriesAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSeriesAnalyzer
+{
+    internal class TheSeriesAnalyzer
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
